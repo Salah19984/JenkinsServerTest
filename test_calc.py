57e956ac05164485e49181fec39bb10df45ff5c7
Sebastian Jaeger <sebastianjaeger1984@hotmail.de>
2021-11-01 14:52:09 +0100

    test_add set to fail for demo purpose

--- test_calc.py
+++ test_calc.py
@@ -7,7 +7,7 @@ class testCalc(unittest.TestCase):
     
     def test_add(self):
         
-        self.assertEqual(calc.add(10, 5,), 15)
+        self.assertEqual(calc.add(10, 5,), 16)
         self.assertEqual(calc.add(-1, 1,), 0)
         self.assertEqual(calc.add(-1, -1,), -2)
         
